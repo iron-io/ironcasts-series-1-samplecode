d859eed8f0f61ee40e81f363ca6cf0e522814313
sidney zhang <sidazhang89@gmail.com>
2013-07-16 13:17:54 -0700

    Init Commit - Rails Boilerplate Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IronWorker101::Application.config.secret_key_base = '0bebc1dd422b1821c3d07f370173136fb793cc4187bfedecfb949ae1558c8c088652bf8579ffd333c0e0bd0866f80dc53f03f622856c1e0f3f143e734cb6aa14'
