8602696e46ff45745c156e8fdc8540ca52aac243
sidney zhang <sidazhang89@gmail.com>
2013-07-16 14:03:01 -0700

    Changing snippet data type from string to text

--- db/migrate/20130716202004_create_snippets.rb
+++ db/migrate/20130716202004_create_snippets.rb
@@ -2,8 +2,8 @@ class CreateSnippets < ActiveRecord::Migration
   def change
     create_table :snippets do |t|
       t.string :language
-      t.string :plain_code
-      t.string :highlighted_code
+      t.text :plain_code
+      t.text :highlighted_code
 
       t.timestamps
     end
