9bb61ac352801b4bd60085ff8d025c489a0df130
sidney zhang <sidazhang89@gmail.com>
2013-07-26 12:58:25 -0700

    introduced mocking code to develop in local environment

--- workers/pygments_worker.rb
+++ workers/pygments_worker.rb
@@ -1,7 +1,10 @@
+require_relative './pygments_worker_mock' unless ARGV.include?('-id')
+require 'uri'
 require 'net/http'
 require 'pg'
 require 'active_record'
 require 'models/snippet'
+
 
 def setup_database
   puts "Database connection details:#{params['database'].inspect}"
@@ -16,4 +19,3 @@ uri = URI.parse("http://pygments.appspot.com/")
 request = Net::HTTP.post_form(uri, lang: params["request"]["lang"], code: params["request"]["code"])
 snippet = Snippet.where(:id => params["snippet_id"]).first
 snippet.update_attribute(:highlighted_code, request.body)
-p snippet
