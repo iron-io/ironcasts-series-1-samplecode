4350ab8412d1c4db0713e97d0f727a9188d12197
sidney zhang <sidazhang89@gmail.com>
2013-08-05 12:41:02 -0700

    modified to latest workflow structure

---
+++ workers/pygments/development/pygments_worker_dev.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+$LOAD_PATH.unshift(File.expand_path('../../../../app/', __FILE__))
+
+def database_config
+  YAML.load(File.open(File.expand_path('../../../../config/database.yml', __FILE__)))
+end
+
+def params
+  {
+    "database" => database_config["development"],
+    "request" => {"lang" => "ruby", "code" => "def hello\n puts 'hello'\n end"},
+    "snippet_id" => 1
+  }
+end
